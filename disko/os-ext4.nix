5c50b05960490a4807c05268dc09a4b1037521ed
YZTangent <yuanzheng.tan.tyz@gmail.com>
2026-05-26 03:00:41 +0800

    feat(disko): add shared os-ext4 and zfs-raid disko modules

---
+++ disko/os-ext4.nix
@@ -0,0 +1,36 @@
+{ ... }: {
+  disko.devices.disk.main = {
+    type = "disk";
+    device = "$DISK_MAIN";
+    content = {
+      type = "gpt";
+      partitions = {
+        boot = {
+          size = "512M";
+          type = "EF00";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
+          };
+        };
+        root = {
+          size = "100%";
+          content = {
+            type = "filesystem";
+            format = "ext4";
+            mountpoint = "/";
+          };
+        };
+        swap = {
+          size = "8G";
+          content = {
+            type = "swap";
+          };
+        };
+      };
+    };
+  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+}
---
+++ disko/zfs-raid.nix
@@ -0,0 +1,56 @@
+{ diskCount ? 4, mode ? "raidz1", poolName ? "tank" }:
+{
+  disko.devices = {
+    disk = builtins.listToAttrs (builtins.genList (i: {
+      name = "data${toString (i + 1)}";
+      value = {
+        type = "disk";
+        device = "$DISK_DATA${toString (i + 1)}";
+        content = {
+          type = "gpt";
+          partitions = {
+            zfs = {
+              size = "100%";
+              content = { type = "zfs"; };
+            };
+          };
+        };
+      };
+    }) diskCount);
+    zpool = {
+      "${poolName}" = {
+        type = "zpool";
+        mode = mode;
+        options = {
+          ashift = "12";
+          autotrim = "on";
+        };
+        rootFsOptions = {
+          compression = "zstd";
+          "com.sun:auto-snapshot" = "false";
+        };
+        disks = builtins.genList (i: "data${toString (i + 1)}") diskCount;
+        datasets = {
+          data = {
+            type = "zfs_fs";
+            mountpoint = "/var/lib/${poolName}/data";
+            options."com.sun:auto-snapshot" = "true";
+          };
+          media = {
+            type = "zfs_fs";
+            mountpoint = "/var/lib/${poolName}/media";
+            options."com.sun:auto-snapshot" = "true";
+          };
+          backups = {
+            type = "zfs_fs";
+            mountpoint = "/var/lib/${poolName}/backups";
+          };
+          containers = {
+            type = "zfs_fs";
+            mountpoint = "/var/lib/${poolName}/containers";
+          };
+        };
+      };
+    };
+  };
+}
