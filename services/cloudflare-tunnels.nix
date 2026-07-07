62c11b50cf372c6893ae9d866ccfe6a809fb39dc
YZTangent <yuanzheng.tan.tyz@gmail.com>
2026-07-07 23:52:13 +0800

    fix(services): address code quality review feedback for cloudflare tunnels module

--- services/cloudflare-tunnels.nix
+++ services/cloudflare-tunnels.nix
@@ -10,10 +10,15 @@ in {
       name = lib.mkOption {
         type = lib.types.str;
         default = "host-${config.networking.hostName}";
+        defaultText = lib.literalExpression ''"host-\${config.networking.hostName}"'';
+        description = "Name of the host tunnel";
+        example = "host-my-server";
       };
       credentialsFile = lib.mkOption {
-        type = lib.types.str;
+        type = lib.types.nullOr lib.types.str;
+        default = null;
         description = "Path to the cloudflared credentials JSON file";
+        example = "/run/secrets/cloudflared-credentials.json";
       };
     };
 
@@ -22,20 +27,36 @@ in {
       name = lib.mkOption {
         type = lib.types.str;
         default = "compute-cluster";
+        description = "Name of the compute tunnel";
+        example = "compute-my-cluster";
       };
       credentialsFile = lib.mkOption {
-        type = lib.types.str;
+        type = lib.types.nullOr lib.types.str;
+        default = null;
         description = "Path to the cloudflared credentials JSON file";
+        example = "/run/secrets/cloudflared-credentials.json";
       };
       ingress = lib.mkOption {
-        type = lib.types.attrsOf lib.types.str;
-        default = { "*" = "http://localhost:80"; };
+        type = lib.types.attrsOf lib.types.anything;
+        default = {};
         description = "Ingress routing rules mapping hostnames to local endpoints";
+        example = { "git.example.com" = "http://localhost:8080"; };
       };
     };
   };
 
   config = lib.mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = !cfg.hostTunnel.enable || cfg.hostTunnel.credentialsFile != null;
+        message = "hostTunnel is enabled but credentialsFile is not set.";
+      }
+      {
+        assertion = !cfg.computeTunnel.enable || cfg.computeTunnel.credentialsFile != null;
+        message = "computeTunnel is enabled but credentialsFile is not set.";
+      }
+    ];
+
     services.cloudflared = {
       enable = true;
       tunnels = lib.mkMerge [
